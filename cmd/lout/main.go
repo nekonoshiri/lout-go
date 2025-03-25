7ee08944d8852ce6f38b5d88ff188c06f3e5c539
nekonoshiri <gexira.halen.toms@gmail.com>
2025-03-25 21:57:03 +0900

    feat: init cmd/lout/main.go file

--- cmd/lout/main.go
+++ cmd/lout/main.go
@@ -1,5 +1,54 @@
 package main
 
-func main() {
+import (
+	"flag"
+	"fmt"
+	"os"
+)
 
+var file string
+
+func init() {
+	flag.StringVar(&file, "f", "", "")
+	flag.StringVar(&file, "file", "", "")
+
+	flag.Usage = func() {
+		fmt.Fprintf(os.Stderr, `Usage: lout -f <state file> command
+
+A low-level runtime for "Lights Out"-like games.
+
+Options:
+  -h, --help     show help
+  -f, --file     specify state file (required)
+
+Commands:
+  init <w> <h>   initialize state file with width w and height h
+  press <x> <y>  press the light at (x, y)
+`)
+	}
+}
+
+func main() {
+	flag.Parse()
+
+	if file == "" {
+		fmt.Fprintln(os.Stderr, "state file is required")
+		flag.Usage()
+		os.Exit(2)
+	}
+
+	switch cmd := flag.Arg(0); cmd {
+	case "init":
+		// TODO
+	case "press":
+		// TODO
+	case "":
+		fmt.Fprintln(os.Stderr, "command is required")
+		flag.Usage()
+		os.Exit(2)
+	default:
+		fmt.Fprintln(os.Stderr, "invalid command:", cmd)
+		flag.Usage()
+		os.Exit(2)
+	}
 }
